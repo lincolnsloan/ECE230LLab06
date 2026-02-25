aa498c61b6f67a46ffc8e658557c5d1102295ff1
lincolnsloan <lincolncolver06@gmail.com>
2026-02-24 17:41:29 -0700

    Added .v files and constraints

---
+++ full_adder.v
@@ -0,0 +1,14 @@
+// Implement module called full_adder
+module full_adder(
+    input A,
+    input B,
+    input Cin,
+    
+    output Y,
+    output Cout
+);
+
+assign Y = (~A & ~B & Cin) | (~A & B & ~Cin) | (A & B & Cin) | (A & ~B & ~Cin);
+assign Cout = (A & B) | (A & Cin) | (B & Cin);
+
+endmodule
